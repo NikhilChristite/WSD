5521fc6aa02507ccc2b72798eee98b0326ec120f
NikhilChristite <141608313+NikhilChristite@users.noreply.github.com>
2023-09-19 15:55:34 +0530

    Delete Lab_Exerice_08.1_CIA-02 directory

--- Lab_Exerice_08.1_CIA-02/Validation.py
+++
@@ -1,9 +0,0 @@
-import lxml.etree as ET
-
-# Load the XML and XSD files
-xml = ET.parse('jokes.xml')
-xsd = ET.parse('jokes.xsd')
-
-# Create a validator and validate the XML data
-validator = ET.XMLSchema(xsd)
-validator.assertValid(xml)
